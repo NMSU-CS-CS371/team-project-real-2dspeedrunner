91fdece6b05d247cad1f78f17d4489d3644d9743
Emmanuel Medrano <emmanuel.medrano@icloud.com>
2026-03-22 22:50:43 -0600

    added a pause menu, moved tile_map.gd into scripts folder

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("ui_cancel") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+func _on_levels_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
