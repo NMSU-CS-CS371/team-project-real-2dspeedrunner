e49f28eec0c5ae837d7939d87b04aa2a268e9bb2
Hunter Morgan <hmorgan5@nmsu.edu>
2026-03-24 21:08:40 -0600

    animation changes, killzone updated

--- scripts/killzone_2.gd
+++ scripts/killzone_2.gd
@@ -1,6 +1,23 @@
 extends Area2D
 
+@onready var timer: Timer = $Timer
 
+#Function is used when character enters killzone
 func _on_body_entered(body: Node2D) -> void:
+	#Prints out msg 'you died'
 	print("You died :(")
+	#Change to half speed 
+	Engine.time_scale = 0.5
+	#Removes CollisionShape2d from body (player) so it falls through the map
+	body.get_node("CollisionShape2D").queue_free()
+	#starts a timer, when it finishes calls other function
+	timer.start()
+	pass # Replace with function body.
+
+#Function is used when timer runs out from prev. function
+func _on_timer_timeout() -> void:
+	#Sets it back to original speed
+	Engine.time_scale = 1
+	#Reloads the scene and restarts the game
+	get_tree().reload_current_scene()
 	pass # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,17 +6,46 @@ const FRICTION = 1000.0       # How fast you stop (higher = snappier stops)
 const SKID_FRICTION = 1500.0 # Extra friction when reversing direction (the "skid")
 const JUMP_VELOCITY = -300.0
 
+#ADDED NEW ACTIONS 
+#jump - spcebar, W, up arrow
+#move_left - left arrow, A
+#move_right - right arrow, D
+#also added crouch?, and double_jump? not implemented yet just added :D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Jump
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
+	if (Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-
+	# Gets the input direction: -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flips the sprite to reflect direction =
+	# Right direction 
+	if direction > 0:
+		animated_sprite.flip_h = false
+	#left direction
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#Play animations
+	#if not moving -> idle animation, else -> run animation
+	#if on floor then -> idle or run, else -> jump animation 
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	# Applies the movement
+	
 	if direction:
 		# Check if we're skidding (moving opposite to input)
 		var is_skidding = sign(velocity.x) != sign(direction) and velocity.x != 0
