5606e024ca8e1c58e0ca91a5e39b705df1cad62f
brookejames303-dev <brooke.james515@gmail.com>
2026-05-06 09:22:30 -0600

    new bkgrd and buttons for lvl select

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -11,19 +11,23 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_level1_pressed() -> void:
+
+
+func _on_lvl1_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	pass # Replace with function body.
 
 
-func _on_level2_pressed() -> void:
+func _on_lvl2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level2.tscn")
-	 # Replace with function body.
+	pass # Replace with function body.
 
 
-func _on_level3_pressed() -> void:
+func _on_lvl3_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level3.tscn")
 	pass # Replace with function body.
 
 
-func _on_back_button_pressed() -> void:
+func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	pass # Replace with function body.
