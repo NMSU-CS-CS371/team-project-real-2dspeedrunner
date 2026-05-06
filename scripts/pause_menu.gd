422a83b5a66a2d5f3fb9cd17b2416f93264eef9b
brookejames303-dev <brooke.james515@gmail.com>
2026-05-06 12:51:14 -0600

    pause menu

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -4,11 +4,7 @@ func _ready():
 	$AnimationPlayer.play("RESET")
 	hide()
 
-func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
-	await $AnimationPlayer.animation_finished
-	hide()
+
 
 func pause():
 	show()
@@ -18,22 +14,31 @@ func pause():
 func testEsc():
 	if Input.is_action_just_pressed("ui_cancel") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("ui_cancel") and get_tree().paused:
-		resume()
+	
 
-func _on_resume_pressed() -> void:
-	resume()
-
-func _on_restart_pressed() -> void:
-	resume()
-	get_tree().reload_current_scene()
-
-func _on_levels_pressed() -> void:
-	resume()
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
 
 func _process(delta):
 	testEsc()
+
+
+func _on_resume2_pressed() -> void:
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	await $AnimationPlayer.animation_finished
+	hide()
+	pass # Replace with function body.
+
+
+func _on_restart2_pressed() -> void:
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
+
+
+func _on_levels2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit2_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
