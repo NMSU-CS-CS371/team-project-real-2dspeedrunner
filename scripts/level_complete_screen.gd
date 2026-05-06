520e0f053ee2f365232fa461f6d5c897165d3cc2
brookejames303-dev <brooke.james515@gmail.com>
2026-05-06 11:05:15 -0600

    more buttons

--- scripts/level_complete_screen.gd
+++ scripts/level_complete_screen.gd
@@ -5,13 +5,20 @@ extends Control
 func _ready() -> void:
 	time_label.text = GameState.final_time
 
-func _on_restart_pressed() -> void:
+
+
+
+
+func _on_restart2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	pass # Replace with function body.
 
 
-func _on_next_level_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
-
-
-func _on_main_menu_pressed() -> void:
+func _on_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	pass # Replace with function body.
+
+
+func _on_next_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	pass # Replace with function body.
